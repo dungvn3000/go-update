bd5e30a1b8d0fd4e7db60ad35de68d4980220cd0
Nguyen Duc Dung <dungvn3000@gmail.com>
2018-11-21 10:30:59 +0700

    Update apply.go
    crypto verification shouldn't mandatory

--- apply.go
+++ apply.go
@@ -101,8 +101,6 @@ func Apply(update io.Reader, opts Options) error {
 		if err = opts.verifySignature(newBytes); err != nil {
 			return err
 		}
-	} else {
-		return errors.New("crypto verification is not optional, you have to specify both keys ")
 	}
 
 	// get the directory the executable exists in
